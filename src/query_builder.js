81efd5921aaf8cb263d6f803c90c3a3d4fbbfbef
Alexander Zobnin <alexanderzobnin@gmail.com>
2017-01-27 14:01:11 +0300

    Fix linter errors.

--- src/query_builder.js
+++ src/query_builder.js
@@ -2,60 +2,60 @@ import _ from 'lodash';
 import dfunc from './dfunc';
 
 export function buildQuery(target, adhocFilters) {
-	let {aggregation, metric, tags, functions} = target;
-	let query = `${aggregation}:${metric}`;
+  let {aggregation, metric, tags, functions} = target;
+  let query = `${aggregation}:${metric}`;
 
-	let functionInstances  = getFunctionInstances(functions);
+  let functionInstances  = getFunctionInstances(functions);
 
-	if ((tags && tags.length) || (adhocFilters && adhocFilters.length)) {
-	  query += '{';
+  if ((tags && tags.length) || (adhocFilters && adhocFilters.length)) {
+    query += '{';
 
-	  if (tags && tags.length) {
-	  	query += tags.join(',');
-	  }
+    if (tags && tags.length) {
+      query += tags.join(',');
+    }
 
-	  if (adhocFilters && adhocFilters.length) {
-	  	let adhocTags = buildAdHocFilterString(adhocFilters);
-	  	if (tags && tags.length) {
-	  		query += ',';
-	  	}
-	  	query += adhocTags;
-	  }
+    if (adhocFilters && adhocFilters.length) {
+      let adhocTags = buildAdHocFilterString(adhocFilters);
+      if (tags && tags.length) {
+        query += ',';
+      }
+      query += adhocTags;
+    }
 
-	  query += '}';
-	} else {
-	  query += '{*}';
-	}
+    query += '}';
+  } else {
+    query += '{*}';
+  }
 
-	if (target.as) {
-	  query += '.' + target.as + '()';
-	}
+  if (target.as) {
+    query += '.' + target.as + '()';
+  }
 
-	var groupedFuncs = _.groupBy(functionInstances, func => {
-	  if (func.def.append) {
-	    return 'appends';
-	  } else {
-	    return 'wraps';
-	  }
-	});
+  var groupedFuncs = _.groupBy(functionInstances, func => {
+    if (func.def.append) {
+      return 'appends';
+    } else {
+      return 'wraps';
+    }
+  });
 
-	_.each(groupedFuncs.appends, func => {
-	  query += '.'  + func.render();
-	});
+  _.each(groupedFuncs.appends, func => {
+    query += '.' + func.render();
+  });
 
-	_.each(groupedFuncs.wraps, func => {
-	  query = func.render(query);
-	});
+  _.each(groupedFuncs.wraps, func => {
+    query = func.render(query);
+  });
 
-	return query;
+  return query;
 }
 
 function getFunctionInstances(functions) {
-	return _.map(functions, func => {
-	  var f = dfunc.createFuncInstance(func.funcDef, {withDefaultParams: false});
-	  f.params = func.params.slice();
-	  return f;
-	});
+  return _.map(functions, func => {
+    var f = dfunc.createFuncInstance(func.funcDef, {withDefaultParams: false});
+    f.params = func.params.slice();
+    return f;
+  });
 }
 
 function buildAdHocFilterString(adhocFilters) {
